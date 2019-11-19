5d5ebac2494e61b8107fb4f059e8a0505d169207
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2019-11-19 17:03:55 +0100

    Connected laravel with vuejs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,12 +14,15 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href=" {{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app"></div>
+    <div id="app">
+        <app></app>
+    </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/bootstrap.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
