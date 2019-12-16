35c1091e282d683d4d0867f37db01db350d308c5
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2019-12-16 12:07:55 +0100

    set up sentry error handler

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/debug-sentry', function () {
+    throw new Exception('My first Sentry error!');
+});
